9d9ea4753158021b476c332923f0f7d68f895dd3
Youngmin Park <ympark1988@gmail.com>
2022-02-09 13:32:19 -0500

    figure generation file for paper

--- generate_figures.py
+++ generate_figures.py
@@ -1,20 +1,98 @@
-from matplotlib import rc
-rc('text',usetex=True)
-rc('font',family='serif', serif=['Computer Modern Roman'])
+import matplotlib as mpl
+#from matplotlib import rc
+#mpl.rc('text',usetex=True)
+#mpl.rc('font',family='serif', serif=['Computer Modern Roman'])
+
+#pgf_with_latex = {"pgf.preamble":"\n".join([r'\usepackage{siunitx}',
+#                                            r'\userpackage{fontenc}'])}
+
+#mpl.rcParams.update(pgf_with_latex)
+mpl.rcParams['font.family'] = 'serif'
+mpl.rcParams['text.usetex'] = True
+mpl.rcParams['pgf.texsystem'] = 'pdflatex'
+mpl.rcParams['text.latex.preamble'] = r'\usepackage{amsmath} \usepackage{siunitx}'
+                  
+
+import mol
 
 import numpy as np
-import matplotlib.pyplot as plt
 
+import os
 import multiprocessing
-
 
 def data_figure():
     """
     figure comparing representative data, average data
     and normalized versions of each
     """
+    import matplotlib.pyplot as plt
 
-    fig = plt.figure()
+    # load normed data
+    d = mol.Data()
+
+    # load unnormed data
+    data_avg_raw, data_rep_raw = d._build_data_dict(L0=d.L0,L=d.L,normed=False)
+
+    
+    #list_hours = ['control','0.5h', '1h','2h','4h','8.5h','24h']
+    list_hours = ['control','0.5h', '1h','2h','24h']
+    #assert(len(list_hours) == len(d.data_avg.keys()))
+    #assert(len(d.data_avg.keys()) == len(d.data_rep.keys()))
+    
+    #fig = plt.figure()
+    fig,axs = plt.subplots(nrows=2,ncols=2,figsize=(8,5))
+    
+    for i,hour in enumerate(list_hours):
+
+        color = str((1-i/len(list_hours))/1.2)
+
+        if hour == 'control':
+            label = '0h'
+        else:
+            label = hour
+
+        if hour == '24h':
+            color = 'tab:blue'
+        else:
+            color = color
+
+        # raw, rep
+        x = data_rep_raw[hour][:,0]
+        y = data_rep_raw[hour][:,1]
+        axs[0,0].plot(x,y,label=label,color=color)
+
+        # raw, avg
+        x = data_avg_raw[hour][:,0]
+        y = data_avg_raw[hour][:,1]
+        axs[0,1].plot(x,y,label=label,color=color)
+        
+        # normed, rep
+        x = d.data_rep[hour][:,0]
+        y = d.data_rep[hour][:,1]
+        axs[1,0].plot(x,y,label=label,color=color)
+
+        # normed, avg
+        x = d.data_avg[hour][:,0]
+        y = d.data_avg[hour][:,1]
+        axs[1,1].plot(x,y,label=label,color=color)
+
+    for i in range(2):
+        for j in range(2):
+            axs[i,j].legend()
+            axs[i,j].set_xlabel(r'Radius ($\si{\um}$)')
+
+
+    axs[0,0].set_title('A. Representative',loc='left')
+    axs[0,1].set_title('B. Average',loc='left')
+    axs[1,0].set_title('C. Representative (Normalized)',loc='left')
+    axs[1,1].set_title('D. Average (Normalized)',loc='left')
+    
+    axs[0,0].set_ylabel('Intensity')
+    axs[0,1].set_ylabel('Intensity')
+    axs[1,0].set_ylabel('Norm. Intensity')
+    axs[1,1].set_ylabel('Norm. Intensity')
+
+    plt.tight_layout()
 
     return fig
 
@@ -37,7 +115,8 @@ def generate_figure(function, args, filenames, title="", title_pos=(0.5,0.95)):
 def main():
 
     figures = [
-        (data_figure, [], ['test.png'])
+        (data_figure, [], ['f_data.png']),
+        (data_figure, [], ['f_data_test_plot.png'])
         ]
 
     # multiprocessing code from Kendrick Shaw
