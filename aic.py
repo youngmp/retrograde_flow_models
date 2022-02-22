eef504a8df3c9e6757a7b1b4f1b30f760ad6443c
Youngmin Park <ympark1988@gmail.com>
2022-02-22 16:07:29 -0500

    added order AIC. not needed anymore though

--- aic.py
+++ aic.py
@@ -13,63 +13,79 @@ In our problem, n = 7 observations.
 """
 
 import numpy as np
-
+import os
 import pde
 
-scenario_numbers = [0,1,2]
+scenario_numbers = [-1,0,1,2,4]
 
 p = pde.PDEModel()
 
-n = 7 # number of observations
+n = 219*7 # number of observations
 
 aicc_data = {}
 aicc_min = 100
 
+fname_list = []
+fname_err_list = []
+model_names = []
+# compile fnames
 for m in scenario_numbers:
+    for order in [1,2]:
+        fname_pre = p.data_dir+'scenario'+str(m)+'_residuals'
+        fname_pre += '_order='+str(order)
+        fname_pre += '_ss'
+        fname = fname_pre + '.txt'
+        fname_err = fname_pre + '_err.txt'
 
-    fname_pre = p.data_dir+'scenario'+str(m)+'_residuals'
-    fname_pre += '_ss'
-    fname = fname_pre + '.txt'
-    fname_err = fname_pre + '_err.txt'
-    
-    residuals = np.loadtxt(fname)
-    err = np.loadtxt(fname_err)
+        print(fname,os.path.isfile(fname))
+        if os.path.isfile(fname) and os.path.isfile(fname):
+            fname_list.append(fname)
+            fname_err_list.append(fname_err)
+            model_names.append(str(m)+'_'+str(order))
+
+for i in range(len(fname_list)):
+
+    residuals = np.loadtxt(fname_list[i])
+    err = np.loadtxt(fname_err_list[i])
     k = len(residuals)
     aicc = n*np.log(err/n) + 2*k*n/(n-k-1)
 
+    m = model_names[i]
     # compile aic data
+
     aicc_data[m] = {}
     aicc_data[m]['res'] = residuals
     aicc_data[m]['err'] = err
-    aicc_data[m]['k'] = k
+    aicc_data[m]['k'] = k+1
     aicc_data[m]['aicc'] = aicc
 
     if aicc < aicc_min:
         aicc_min = aicc
 
+
 aicc_data['min'] = aicc_min
 
 # compute delta
-for m in scenario_numbers:
+for m in model_names:    
     aicc_data[m]['delta'] = aicc_data[m]['aicc'] - aicc_data['min']
 
 # compute weights
-for i in scenario_numbers:
+for m in model_names:
     norm = 0
-    for r in scenario_numbers:
+    for r in model_names:
         delta_r = aicc_data[r]['delta']
         norm += np.exp(-delta_r/2)
 
     
-    delta_i = aicc_data[i]['delta']
+    delta_i = aicc_data[m]['delta']
     
-    aicc_data[i]['w'] = np.exp(-delta_i/2)/norm
-
+    aicc_data[m]['w'] = np.exp(-delta_i/2)/norm
     
 print('{:10s} {:10s} {:10s} {:10s}'.format('scenario','k_i','aicc_i','w_i'))
-for m in scenario_numbers:
+for m in model_names:
     k = aicc_data[m]['k']
     aicc = aicc_data[m]['aicc']
     delta = aicc_data[m]['w']
-    s = '{:<10.1f} {:<10.1f} {:<10.1f} {:<10.1f}'
+    s = '{:<10s} {:<10.0f} {:<10.4f} {:<10.4f}'
     print(s.format(m,k,aicc,delta))
+    #print('add +1 to k')
