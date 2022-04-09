49a1cdbe30614e30320e1ac93c32bdd98b81cbc3
youngmp <ympark1988@gmail.com>
2022-04-09 13:46:17 -0500

    now compatible with outputs from model_fitting

--- aic.py
+++ aic.py
@@ -7,85 +7,93 @@ $\text{AIC}_i = n\log(\text{RSS}_i/n) + 2k_i$,
 where $n$ is the total number of observations, $k_i$ is the number of estimated parameters for model $i$, $\text{RSS}_i$ is the minimum value of the objective/cost function.
 
 In our case, we need to use the corrected AIC (small number of observations k > n/40):
-$\text{AICc}_i = n\log(\text{RSS}_i/n) + 2 k_i n/(n-k_i-1)$,
+$\text{Aic}_i = n\log(\text{RSS}_i/n) + 2 k_i n/(n-k_i-1)$,
 
-In our problem, n = 7 observations.
+In our problem, n = (the number of spatial points in data) * (number to time observations).
 """
+
+import pde
+import lib
 
 import numpy as np
 import os
-import pde
 
-scenario_numbers = [-1,0,1,2,4]
+
+models = ['t1a','t1b','t1c','t1d','t1e',
+          't2a','t2b','t2c','t2d',
+          'jamminga','jammingb','jammingc','jammingd']
+
+#models = ['t1a','t1b','t1c','t1d']
 
 p = pde.PDEModel()
 
-n = 219*7 # number of observations
 
-aicc_data = {}
-aicc_min = 100
+# count number of observations
+n = 0
+for key in p.data_avg.keys():
+    n += len(p.data_avg[key][:,0])
+    print(len(p.data_avg[key][:,0]),key)
 
-fname_list = []
-fname_err_list = []
-model_names = []
+print('number of observations',n)
+
+aic_data = {}
+aic_min = 100
+
 # compile fnames
-for m in scenario_numbers:
-    for order in [1]:
-        fname_pre = p.data_dir+'scenario'+str(m)+'_residuals'
-        fname_pre += '_order='+str(order)
-        fname_pre += '_ss'
-        fname = fname_pre + '.txt'
-        fname_err = fname_pre + '_err.txt'
+#for model in models:
 
-        print(fname,os.path.isfile(fname))
-        if os.path.isfile(fname) and os.path.isfile(fname):
-            fname_list.append(fname)
-            fname_err_list.append(fname_err)
-            model_names.append(str(m)+'_'+str(order))
+#    fname_list.append(lib.get_parameter_fname(model))
+    #if os.path.isfile(fname) and os.path.isfile(fname):
+    #    fname_list.append(fname)
+    #    fname_err_list.append(fname_err)
+    #    model_names.append(str(model)+'_'+str(order))
 
-for i in range(len(fname_list)):
+for i in range(len(models)):
+    
+    err, min_seed = lib.lowest_error_seed(models[i])    
+    residuals = np.loadtxt(lib.get_parameter_fname(models[i],min_seed))
 
-    residuals = np.loadtxt(fname_list[i])
-    err = np.loadtxt(fname_err_list[i])
+    #print(models[i],'err',err,min_seed)
+    err = 10**err
+    
+
     k = len(residuals)
-    aicc = n*np.log(err/n) + 2*k*n/(n-k-1)
+    aic = n*np.log(err/n) + 2*k#*n/(n-k-1)
 
-    m = model_names[i]
     # compile aic data
 
-    aicc_data[m] = {}
-    aicc_data[m]['res'] = residuals
-    aicc_data[m]['err'] = err
-    aicc_data[m]['k'] = k+1
-    aicc_data[m]['aicc'] = aicc
+    aic_data[models[i]] = {}
+    aic_data[models[i]]['res'] = residuals
+    aic_data[models[i]]['err'] = err
+    aic_data[models[i]]['k'] = k+1
+    aic_data[models[i]]['aic'] = aic
 
-    if aicc < aicc_min:
-        aicc_min = aicc
+    if aic < aic_min:
+        aic_min = aic
 
-
-aicc_data['min'] = aicc_min
+aic_data['min'] = aic_min
 
 # compute delta
-for m in model_names:    
-    aicc_data[m]['delta'] = aicc_data[m]['aicc'] - aicc_data['min']
+for m in models:
+    aic_data[m]['delta'] = aic_data[m]['aic'] - aic_data['min']
+    #print(m,aic_data[m]['delta'])
 
 # compute weights
-for m in model_names:
+for m in models:
     norm = 0
-    for r in model_names:
-        delta_r = aicc_data[r]['delta']
+    for r in models:
+        delta_r = aic_data[r]['delta']
         norm += np.exp(-delta_r/2)
-
+        #print(norm)
     
-    delta_i = aicc_data[m]['delta']
+    delta_i = aic_data[m]['delta']
+    aic_data[m]['w'] = np.exp(-delta_i/2)/norm
     
-    aicc_data[m]['w'] = np.exp(-delta_i/2)/norm
-    
-print('{:10s} {:10s} {:10s} {:10s}'.format('scenario','k_i','aicc_i','w_i'))
-for m in model_names:
-    k = aicc_data[m]['k']
-    aicc = aicc_data[m]['aicc']
-    delta = aicc_data[m]['w']
+print('{:10s} {:10s} {:10s} {:10s}'.format('scenario','k_i','aic_i','w_i'))
+for m in models:
+    k = aic_data[m]['k']
+    aic = aic_data[m]['aic']
+    delta = aic_data[m]['w']
     s = '{:<10s} {:<10.0f} {:<10.4f} {:<10.4f}'
-    print(s.format(m,k,aicc,delta))
+    print(s.format(m,k,aic,delta))
     #print('add +1 to k')
