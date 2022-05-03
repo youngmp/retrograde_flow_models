ab27c6d87e7ee102fed727a7ebab355a9edea757
youngmp <ympark1988@gmail.com>
2022-05-03 14:56:39 -0500

    reduced visit to reduce annealing time

--- model_fitting.py
+++ model_fitting.py
@@ -171,7 +171,7 @@ def get_data_residuals(p,par_names=['eps','df','dp'],
     if method == 'annealing':
         
         res = dual_annealing(cost_fn,bounds=bounds,args=args,
-                             visit=2.9,restart_temp_ratio=1e-07,
+                             visit=2.75,restart_temp_ratio=1e-07,
                              initial_temp=6e4,accept=-5,seed=seed,
                              maxiter=5000,maxfun=1e8,
                              local_search_options={'method':'Nelder-Mead','bounds':bounds})
